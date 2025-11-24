14297378cd9c5ae8cc11cbb9cf033bd78c23778b
leo050225 <leo050225@gmail.om>
2025-11-24 14:33:43 +0800

    初始化

--- app/src/main/java/s1131244o365st/pu/edu/firestore/MainActivity.kt
+++ app/src/main/java/s1131244o365st/pu/edu/firestore/MainActivity.kt
@@ -1,6 +1,8 @@
 package s1131244o365st.pu.edu.firestore
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +13,39 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import s1131244o365st.pu.edu.firestore.ui.theme.FireStoreTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val db = Firebase.firestore
+
+        // 建立一個包含姓與名的新使用者
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        // 新增一個具有自動產生 ID 的文件
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
         setContent {
             FireStoreTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
